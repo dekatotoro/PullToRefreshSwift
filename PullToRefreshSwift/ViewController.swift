2e1f2c4557058e91ea3dd4f0150568c28900b09b
hato_yuji <nekoganeru@gmail.com>
2014-12-08 15:25:17 +0900

    Initial Commit

---
+++ PullToRefreshSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PullToRefreshSwift
+//
+//  Created by 波戸 勇二 on 12/8/14.
+//  Copyright (c) 2014 Yuji Hato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PullToRefreshSwiftTests/PullToRefreshSwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  PullToRefreshSwiftTests.swift
+//  PullToRefreshSwiftTests
+//
+//  Created by 波戸 勇二 on 12/8/14.
+//  Copyright (c) 2014 Yuji Hato. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class PullToRefreshSwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
