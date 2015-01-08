4beb57c7dd040bd47cf33403671443a6c21bfc09
hato_yuji <nekoganeru@gmail.com>
2015-01-08 11:23:13 +0900

    Default constants changed.
    fixedTop changed true.

--- Source/PullToRefreshConst.swift
+++ Source/PullToRefreshConst.swift
@@ -11,7 +11,7 @@ struct PullToRefreshConst {
     static let imageName: String = "pulltorefresharrow.png"
     static let height: CGFloat = 80
     static let duration: Double = 0.5
-    static let fixedTop = false
+    static let fixedTop = true
     static let alpha = true
     static let tag = 810
 }
